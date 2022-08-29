178d7384d85f3925bdb6413b93dc52e6e845683f
Sebastian Fritzsche <sebifr58@gmx.de~>
2022-08-29 08:12:59 +0200

    update Story

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 btn.addEventListener('click', function () {
 
+    // Global variables
     var btn = document.getElementById('btn');
+    var choice1;
+    var choice2;
+    var choice3;
+    var choice4;
+    var choice5;
     
 
     // Player Blueprint
@@ -15,21 +21,53 @@ btn.addEventListener('click', function () {
 
         var player = new Player(playername);
 
-        confirm('Hallo' + player.name + '. ');
+        confirm('Hallo ' + player.name + '. ');
     }
     CreatePlayer();
 
     // first dialogue with the hut owner -> HO
     function FirstDialogueHO() {
-        var choice1 = prompt('Choice1');
+        choice1 = confirm('Du wachst in einer verfallen Hütte auf einem Bett auf. An einem Tisch sitzt ein älterer Mann.');
+        choice1 = prompt('Der Mann scheint nicht zu bemerken dass du auf gewacht bist. Du kannst etwas zu ihm sagen. Mögliche Texte/Fragen: - Hallo; - Wo bin ich hier?')
+        if (choice1 === 'Hallo') {
+            choice2 = prompt('Hallo kleines Wesen. Du wurdest ausgewählt. Mögliche Fragen: - Für was ausgewählt?');
+        }else if (choice1 === 'Wo bin ich hier?') {
+            hoice2 = prompt('Hallo kleines Wesen. Das tut nichst zur Sache. Du wurdest ausgewählt. Mögliche Fragen: - Für was ausgewählt?')
+        }
 
-        
+        if (choice2 === 'Für was ausgewählt?') {
+            choice2 = confirm('Plötzlich löst sich der alte Mann in Luft auf und du bist alleine in der Hütte. ')
+            choice3 =  prompt('Du versucht durch die Tür zu gehen, aber sie ist verschlossen. Mögliche Befehle: - Untersuchen');
+        }   
     }
     FirstDialogueHO();
 
+    // this function describe the examine Hut
+    function ExamineHut() {
+        switch(choice3) {
+            case 'Untersuchen':
+                choice4 = prompt('Du findest auf dem Holzboden einen Wurm und in der Ecke steht eine Angelrute. Du hast jetzt eine Angelrute und einen Wurm in deinem Inventar. Leider keinen Schlüssel. Um raus zu kommen musst du die Tür wohl auframen. Mögliche Befehle: - Auframen');
+        }
+    }
+    ExamineHut();
+
+    // open the hut door 
+    function openDoor() {
+        if (choice4 === 'Auframen') {
+            for (var i = 0; i <= 3; i++) {
+                if (i < 3) {
+                    choice5 = confirm('Noch mal probieren');
+                }else if (i === 3) {
+                    choice5 = prompt('offen')
+                }
+            }
+        }
+    }
+    openDoor();
+
     // innfront of the hut
     function StoryInfrontOfHut() {
-        var choice2 = prompt('choice2');
+        
 
     }
     StoryInfrontOfHut();
